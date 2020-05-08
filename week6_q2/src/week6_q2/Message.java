18f85a308e73843a84c38ab973e4ec47f8cdd248
Mithun p <33917786+mithunputhusseri@users.noreply.github.com>
2020-05-08 13:34:23 +0530

    week6 assignment

---
+++ week6_q2/src/week6_q2/Message.java
@@ -0,0 +1,31 @@
+package week6_q2;
+
+public class Message implements Runnable{
+	private static final int N = 10;
+    private Thread thread;
+    private static Object object = new Object();
+
+    public Message(String name){
+        thread = new Thread(this, name);
+        thread.start();
+    }
+
+    public void run(){
+        for(int i=0; i<N; i++){
+            synchronized (object) {
+                System.out.println(thread.getName());
+                object.notify();
+                try {
+                    object.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+}
+
+
+
+
---
+++ week6_q2/src/week6_q2/main.java
@@ -0,0 +1,11 @@
+package week6_q2;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Message message1 = new Message("tic");
+		Message message2 = new Message("toc");
+	}
+
+}
